52420f9120e7ff60441dfafb4bfa1134a1187648
owolfdev <owolfdev@gmail.com>
2023-08-05 05:19:39 +0700

    updates

--- components/chat.tsx
+++ components/chat.tsx
@@ -34,37 +34,10 @@ type ChatMessage = {
   updated_at: string;
 };
 
-const ChatView = ({ data }: { data: ChatMessage[] }) => {
-  const userContext = useUser();
-  const userId = userContext ? userContext.user?.id : null;
-
-  return (
-    <div className="w-full border rounded-lg h-[400px] overflow-y-scroll p-4 flex flex-col">
-      {data.map((item, index) => (
-        <div
-          key={index}
-          className={`w-full mb-2 flex items-start ${
-            userId === item.sender_id ? "justify-end" : "justify-start"
-          }`}
-        >
-          <div
-            className={`${
-              userId === item.sender_id
-                ? "bg-blue-500 text-white"
-                : "bg-gray-300 text-black"
-            } rounded-2xl p-2 break-words inline-flex text-sm`}
-            style={{ maxWidth: "75%" }}
-          >
-            {item.content}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 export function Chat({ supabase }: { supabase: any }) {
   const [data, setData] = useState<ChatMessage[]>([]);
+  const [avatars, setAvatars] = useState<Record<string, string>>({});
+  const [newMessage, setNewMessage] = useState<string>("");
   const userContext = useUser();
 
   const user = userContext ? userContext.user : null;
@@ -80,17 +53,117 @@ export function Chat({ supabase }: { supabase: any }) {
     return data;
   };
 
+  const loadAvatars = async () => {
+    const senderIds = Array.from(new Set(data.map((item) => item.sender_id))); // Gets unique sender
+    for (let id of senderIds) {
+      const { data } = await supabase
+        .from("profiles")
+        .select("avatar")
+        .eq("user_id", id);
+      if (data && data.length > 0) {
+        setAvatars((prevAvatars) => ({
+          ...prevAvatars,
+          [id]: data[0].avatar,
+        }));
+      }
+    }
+  };
+
   useEffect(() => {
     getData();
   }, []);
 
   useEffect(() => {
-    console.log("data", data);
+    loadAvatars();
   }, [data]);
 
-  const handleSendMessage = (e: any) => {
+  const handleSendMessage = async (e: any) => {
     e.preventDefault();
-    console.log("send message");
+    console.log("send message:", user?.id, newMessage);
+    const { data, error } = await supabase
+      .from("chat_messages")
+      .insert({
+        sender_id: user?.id, // replace with the actual sender_id
+        content: newMessage,
+      })
+      .single();
+
+    setNewMessage(""); // Clear the input after sending
+  };
+
+  useEffect(() => {
+    if (data) {
+      setData(data);
+    }
+
+    const channel = supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "chat_messages",
+        },
+        (payload: any) => setData((messages) => [...messages, payload.new])
+        //(payload: any) => console.log("payload:", payload)
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [data]);
+
+  const ChatView = ({ data }: { data: ChatMessage[] }) => {
+    const userContext = useUser();
+    const userId = userContext ? userContext.user?.id : null;
+
+    const sortedData = [...data].sort(
+      (a, b) => new Date(b.sent_at).getTime() - new Date(a.sent_at).getTime()
+    );
+
+    return (
+      <div className="w-full border rounded-lg h-[400px] overflow-y-scroll p-4 flex flex-col">
+        {sortedData.map((item, index) => (
+          <div
+            key={index}
+            className={`w-full mb-2 flex items-start ${
+              userId === item.sender_id ? "justify-end" : "justify-start"
+            }`}
+          >
+            <div style={{ maxWidth: "75%" }} className="flex gap-1 items-end">
+              {userId !== item.sender_id && (
+                <div className="w-6 h-6 bg-cover bg-center rounded-full overflow-hidden flex-shrink-0">
+                  <img src={avatars[item.sender_id] || ""} />
+                </div>
+              )}
+              <div>
+                <div className="text-[11px] text-gray-300 pr-3 pl-3  text-right ">
+                  {new Date(item.sent_at).toLocaleString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    // year: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  })}
+                </div>
+                <div
+                  className={`${
+                    userId === item.sender_id
+                      ? "bg-gray-600 text-white"
+                      : "bg-gray-300 text-black"
+                  } rounded-2xl px-3 py-2 break-words text-sm`}
+                >
+                  {item.content}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   return (
@@ -102,11 +175,14 @@ export function Chat({ supabase }: { supabase: any }) {
         </span>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSendMessage}>
           <div className="flex flex-col gap-2">
-            <Input placeholder="Enter your message" />
+            <Input
+              placeholder="Enter your message"
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
             <div>
-              <Button onClick={handleSendMessage}>Send</Button>
+              <Button type="submit">Send</Button>
             </div>
           </div>
         </form>
